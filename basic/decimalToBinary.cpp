1ea10580e99d997e1b563fd7887de2bf36ada5d4
Roshan Patel <roshanpatel12309@gmail.com>
2025-07-12 12:07:59 +0545

    Solved

--- basic/decimalToBinary.cpp
+++ basic/decimalToBinary.cpp
@@ -1,6 +1,20 @@
  #include<iostream>
+ #include<math.h>
  using namespace std;
 
  int main(){
-    
+    int power = 0 , decimal, binary=0;
+    // Taking input for decimal number
+    cout << "Enter Decimal Number : ";
+    cin >> decimal;
+    int dec = decimal;
+
+    while(decimal > 0){
+       int rem = decimal % 2;
+       binary += rem*pow(10,power);
+       decimal /= 2;
+       power++;
+    }
+
+    cout << "Binary no. of " << dec << " base ten : " << binary << endl;
  }
