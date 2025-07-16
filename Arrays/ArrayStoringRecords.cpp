18e1abeaaf56a18232ebddcca84b1c15ad193fdf
Roshan Patel <roshanpatel12309@gmail.com>
2025-07-16 14:46:03 +0545

    Store as much of datas in array and display it

--- Arrays/ArrayStoringRecords.cpp
+++ Arrays/ArrayStoringRecords.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int record() {
-    int n;
-    cout << "Enter the number of records: ";
-    cin >> n;
-
+int record(int num) {
     string name[40];
-
-    for (int i = 0; i < n; i++) { 
+    for (int i = 0; i < num; i++) { 
         cout << "Enter name " << i + 1 << ": ";
         cin >> name[i];
     }
 
     cout << "\nYou entered:\n";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < num; i++) {
         cout << name[i] << endl;
     }
 
@@ -22,5 +17,8 @@ int record() {
 }
 
 int main() {
-    return record();
+     int n;
+    cout << "Enter the number of records: ";
+    cin >> n;
+    return record(n);
 }
