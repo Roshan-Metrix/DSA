5bc381afc47eef805c0244667b04ad2302bf8c1a
Roshan Patel <roshanpatel12309@gmail.com>
2025-07-17 13:43:43 +0545

    Finding smallest number in Arrays

--- Arrays/smallestAndLargest.cpp
+++ Arrays/smallestAndLargest.cpp
@@ -1,29 +1,27 @@
+// Finding smallest number from array
 #include<iostream>
 using namespace std;
 
-// int smallest(int num){
-    
-// }
-
-// int largest(){
-
-// }
+int smallestNumber(int num,int records[]){
+    int min = records[0];
+    for(int i=0;i<num;i++){
+        if(records[i] < min){
+            min = records[i];
+        }
+    }
+    return min;
+}
 
 int main(){
     int n;
      cout << "Enter the no. of datas for Array:";
      cin >> n;
 
-     int datas[40];
-
-     cout << "\nEnter " << n << "datas:\n" << endl;
+     int datas[n];
+     cout << "\nEnter " << n << " datas:\n" << endl;
      for(int i=0;i<n;i++){
         cin >> datas[i];
      }
-      cout << '\n' << endl;
-     for(int i=0;i<n;i++){
-        cout << datas[i] << endl ;
-     }
-     
-    //  return smallest(n);
+    cout << "Smallest Among them is "<< smallestNumber(n, datas);
+    return 0;
 }
