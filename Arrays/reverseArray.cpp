d660a22374eb6a76b7a51f8057f8c5838c046ec9
Roshan Patel <roshanpatel12309@gmail.com>
2025-07-18 11:30:28 +0545

    Reverse any array datas

--- Arrays/reverseArray.cpp
+++ Arrays/reverseArray.cpp
@@ -1,12 +1,26 @@
 #include<iostream>
 using namespace std;
 
-int reverseDatas(int noOfDatas,int records[]){
-    
+// Logic To Reverse Datas Of Arrays
+
+void reverseDatas(int noOfDatas,int records[]){
+    int temp;
+    for(int i=0;i<noOfDatas;i++){
+        for(int j=i+1;j<noOfDatas;j++){
+            temp = records[i];
+            records[i] = records[j];
+            records[j] = temp;
+        }
+    }
+    for(int i=0;i<noOfDatas;i++){
+           cout << records[i] << ' ';
+    }
 }
 
+// Main function
+ 
 int main(){
-      int n, num;
+      int n;
     cout << "Enter the no. of datas for Array:";
     cin >> n;
 
@@ -15,5 +29,6 @@ int main(){
     for (int i = 0; i < n; i++){
         cin >> datas[i];
     }
-    cout << "The reverse datas is " << reverseDatas(n,datas);
+    cout << "The reverse datas is ";
+     reverseDatas(n, datas);
 }
