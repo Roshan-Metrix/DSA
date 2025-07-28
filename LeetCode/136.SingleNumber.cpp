61abd45921aea01b5b512e8bce6f51f43434880b
Roshan Patel <roshanpatel12309@gmail.com>
2025-07-28 11:16:39 +0545

    Array problem no. 136 Single Number soln

---
+++ LeetCode/136.SingleNumber.cpp
@@ -0,0 +1,26 @@
+// We can use Bitwise XOR logic to solve this problem i.e 
+//    0 ^ 0 -> 0 
+//    1 ^ 1 -> 0
+//    0 ^ 1 -> 1
+//    1 ^ 0 -> 1
+//    n ^ n -> 0   { where n is any integer }
+//    n ^ 0 -> n
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int singleNumber(vector<int>& nums) {
+    int result = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        result ^= nums[i];
+    }
+    return result;
+}
+    
+
+int main(){
+    vector<int> nums = {2, 2, 1};
+    int result = singleNumber(nums);
+    cout << result << endl;
+}
