7550f296af0d7211cf3c526ec58201677bff1b59
Roshan Patel <roshanpatel12309@gmail.com>
2025-09-13 08:32:10 +0530

    LeetCode Problem no.21 solved

--- LeetCode/21.MergedTwoSortedLists.cpp
+++ LeetCode/21.MergedTwoSortedLists.cpp
@@ -39,3 +39,17 @@ int main() {
     ListNode* merged = mergeTwoLists(list1, list2);
     printList(merged); // Output: 1 2 3 4 5 6
 }
+
+
+// Or use class
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         if (!list1 || !list2)
+//       return list1 ? list1 : list2;
+//     if (list1->val > list2->val)
+//       swap(list1, list2);
+//     list1->next = mergeTwoLists(list1->next, list2);
+//     return list1;
+//     }
+// };
