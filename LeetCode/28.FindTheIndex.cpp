fe7010fe9e846dc33a636fd419e4bc852fa559aa
Roshan Patel <roshanpatel12309@gmail.com>
2025-09-13 08:41:32 +0530

    LeetCode problem no. 28 solved

---
+++ LeetCode/28.FindTheIndex.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int strStr(string haystack, string needle) {
+        const int m = haystack.length();
+    const int n = needle.length();
+
+    for (int i = 0; i < m - n + 1; i++)
+      if (haystack.substr(i, n) == needle)
+        return i;
+
+    return -1;
+    }
+
+    int main(){
+      string haystack = "sadbutsad";
+      string needle = "sad";
+      cout << strStr(haystack,needle);
+    }
