f024e44fbc9534d7e6f431fc97c6a0d30a10ed30
Roshan Patel <roshanpatel12309@gmail.com>
2025-09-13 09:17:33 +0530

    #67 Leetcode Problem solved

---
+++ LeetCode/67.AddBinary.cpp
@@ -0,0 +1,30 @@
+  #include<iostream>
+#include<vector>
+#include<ranges>
+
+using namespace std;
+  
+  string addBinary(string a, string b) {
+      string ans;
+    int carry = 0;
+    int i = a.length() - 1;
+    int j = b.length() - 1;
+
+    while (i >= 0 || j >= 0 || carry) {
+      if (i >= 0)
+        carry += a[i--] - '0';
+      if (j >= 0)
+        carry += b[j--] - '0';
+      ans += carry % 2 + '0';
+      carry /= 2;
+    }
+
+    ranges::reverse(ans);
+    return ans;  
+    }
+
+    int main(){
+        string a="11";
+        string b="1";
+        cout << addBinary(a,b);
+    }
