d5ddcb69899576adf6ffc529fd8fad63cc696e0b
Roshan Patel <roshanpatel12309@gmail.com>
2025-09-13 08:28:37 +0530

    LeetCode Problem no.21 solved

--- LeetCode/21.MergedTwoSortedLists.cpp
+++ LeetCode/21.MergedTwoSortedLists.cpp
@@ -1,7 +1,41 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout << << endl;
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    if (!list1 || !list2)
+        return list1 ? list1 : list2;
+    if (list1->val > list2->val)
+        swap(list1, list2);
+    list1->next = mergeTwoLists(list1->next, list2);
+    return list1;
+}
+
+// Helper to print the list
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    // List 1: 1 -> 3 -> 5
+    ListNode* list1 = new ListNode(1);
+    list1->next = new ListNode(3);
+    list1->next->next = new ListNode(5);
+
+    // List 2: 2 -> 4 -> 6
+    ListNode* list2 = new ListNode(2);
+    list2->next = new ListNode(4);
+    list2->next->next = new ListNode(6);
+
+    ListNode* merged = mergeTwoLists(list1, list2);
+    printList(merged); // Output: 1 2 3 4 5 6
 }
