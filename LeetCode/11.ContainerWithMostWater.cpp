0056e02fb7706c43c00539d6a2dc1c23fb0362d9
Roshan Patel <roshanpatel12309@gmail.com>
2025-09-28 13:35:59 +0530

    #11 no. LeetCode problem solved

---
+++ LeetCode/11.ContainerWithMostWater.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+ int maxArea(vector<int>& height) {
+         int ans = 0;
+    int l = 0;
+    int r = height.size() - 1;
+
+    while (l < r) {
+      const int minHeight = min(height[l], height[r]);
+      ans = max(ans, minHeight * (r - l));
+      if (height[l] < height[r])
+        ++l;
+      else
+        --r;
+    }
+
+    return ans;
+    }
+
+    int main(){
+        vector<int> height = {1,8,6,2,5,4,8,3,7};
+        cout << maxArea(height);
+    }
