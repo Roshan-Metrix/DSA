86e1016042ee5ebcc75fb44439330b22a6bef3f9
Roshan Patel <roshanpatel12309@gmail.com>
2025-10-01 08:45:56 +0530

    #976 no. LeetCode solved

---
+++ LeetCode/976.LargestPerimeterTriangle.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int largestPerimeter(vector<int> &nums)
+{
+    ranges::sort(nums);
+    // or
+// std::sort(nums.begin(), nums.end())
+    for (int i = nums.size() - 1; i > 1; --i)
+        if (nums[i - 2] + nums[i - 1] > nums[i])
+            return nums[i - 2] + nums[i - 1] + nums[i];
+
+    return 0;
+}
+
+int main()
+{
+    vector<int> nums = {2, 1, 2};
+    cout << largestPerimeter(nums);
+}
