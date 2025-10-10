dc7ca37724d669582c8d4e9dcab1719bc5987fc7
Roshan Patel <roshanpatel12309@gmail.com>
2025-10-10 06:20:01 +0530

    #3147 no. LeetCode solved

---
+++ LeetCode/3147.TakingMaximumEnergyFromTheMysticDungeon.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include<ranges>
+
+using namespace std;
+  
+  int maximumEnergy(vector<int>& energy, int k) {
+   vector<int> dp = (energy);
+   for(int i = energy.size()-1-k;i<=0;--i){
+    dp[i] += dp[i+k];
+    return ranges::max(dp);
+   }
+}
+
+ int main(){
+    vector<int> energy = {5,2,-10,-5,1};
+    int k = 3;
+    cout << maximumEnergy(energy,k);
+ }   
