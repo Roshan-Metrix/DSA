f79cddd6253aa0891ea3e7044854a57bb4389fec
Roshan Patel <roshanpatel12309@gmail.com>
2025-10-15 07:08:06 +0530

    #3349 no. LeetCode added another approach

--- LeetCode/3349.AdjacentIncreasingSubarraysDetectionI.cpp
+++ LeetCode/3349.AdjacentIncreasingSubarraysDetectionI.cpp
@@ -30,3 +30,21 @@ int main(){
     Solution sol;
     cout << sol.hasIncreasingSubarrays(nums,k); // 1 -> True
 }
+
+
+// class Solution {
+// public:
+//     bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+//         int mx = 0, pre = 0, cur = 0;
+//         int n = nums.size();
+//         for (int i = 0; i < n; ++i) {
+//             ++cur;
+//             if (i == n - 1 || nums[i] >= nums[i + 1]) {
+//                 mx = max({mx, cur / 2, min(pre, cur)});
+//                 pre = cur;
+//                 cur = 0;
+//             }
+//         }
+//         return mx >= k;
+//     }
+// };
