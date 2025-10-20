2469a8c20da2dffaf0d1cdc043d86e1df1b89324
Roshan Patel <roshanpatel12309@gmail.com>
2025-10-20 14:27:03 +0530

    #189 no. LeetCode solved

---
+++ LeetCode/189.RotateArray.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution{
+    public:
+      void rotate(vector<int>& nums, int k){
+         int n = nums.size();
+         if(n == 0) return;
+         k %= n;
+         if(k == 0) return;
+         reverse(nums.begin(), nums.end());
+         reverse(nums.begin(), nums.begin() + k);
+         reverse(nums.begin() + k, nums.end());
+      }
+};
+
+int main(){
+    vector<int> nums = {1,2,3,4,5,6,7};
+    int k = 3;
+    Solution sol;
+    sol.rotate(nums, k);
+    for(int n : nums){
+        cout <<  n << ",";
+    }
+    cout << endl;
+    return 0;
+}
