bd3f1f6fbcfbb707082552c334932962f730d040
Roshan Patel <roshanpatel12309@gmail.com>
2025-10-20 21:28:02 +0530

    #10 no. LeetCode solved

---
+++ LeetCode/10.RegularExpressionMatching.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+using namespace std;
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size(), n = p.size();
+        bool f[m + 1][n + 1];
+        memset(f, false, sizeof f);
+        f[0][0] = true;
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p[j - 1] == '*') {
+                    f[i][j] = f[i][j - 2];
+                    if (i && (p[j - 2] == '.' || p[j - 2] == s[i - 1])) {
+                        f[i][j] |= f[i - 1][j];
+                    }
+                } else if (i && (p[j - 1] == '.' || p[j - 1] == s[i - 1])) {
+                    f[i][j] = f[i - 1][j - 1];
+                }
+            }
+        }
+        return f[m][n];
+    }
+};
+
+int main() {
+    Solution sol;
+    string s = "aab";
+    string p = "c*a*b";
+    cout << (sol.isMatch(s, p) ? "true" : "false") << endl; // Output: true
+    return 0;
+}
