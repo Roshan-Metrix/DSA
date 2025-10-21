06e9fbbaee730d72bc1355d79e72ba22759f2377
Roshan Patel <roshanpatel12309@gmail.com>
2025-10-21 09:14:46 +0530

    #50 , attempted but error

--- LeetCode/50.Pow.cpp
+++ LeetCode/50.Pow.cpp
@@ -3,18 +3,25 @@
 #include<iostream>
 using namespace std;
 
-double myPow(double x, int n) {
-    double result = 1;
-    long m = 1;
-    if(n < 0) {
-        x = 1/x;
-        m = -m;
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double result = 1;
+        long long power = n;
+
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        for (int i = 0; i < power; ++i) {
+            result *= x;
+        }
+
+        return result;
     }
-   for(int i=1;i<=n;i++){
-      result *= x;
-   }
-   return result;
-}
+};
+
 
 int main(){
     double base = 2;
