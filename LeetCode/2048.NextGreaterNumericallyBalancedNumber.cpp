fb968285b371f8c2e9e757bce9b47eb248994444
Roshan Patel <roshanpatel12309@gmail.com>
2025-10-24 05:59:57 +0530

    #2048 no. LeetCode solved

---
+++ LeetCode/2048.NextGreaterNumericallyBalancedNumber.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int nextBeautifulNumber(int n) {
+        for (int x = n + 1;; ++x) {
+            int cnt[10]{};
+            for (int y = x; y > 0; y /= 10) {
+                ++cnt[y % 10];
+            }
+            bool ok = true;
+            for (int y = x; y > 0; y /= 10) {
+                if (y % 10 != cnt[y % 10]) {
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok) {
+                return x;
+            }
+        }
+    }
+};
+
+int main() {
+    Solution sol;
+    int n = 1;
+    cout << sol.nextBeautifulNumber(n) << endl; // Output: 22
+    return 0;
+}
