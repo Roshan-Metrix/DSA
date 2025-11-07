cb48fb75f28ea74912ae845fa28aa5f11a0bdb39
Roshan Patel <roshanpatel12309@gmail.com>
2025-11-07 14:33:06 +0530

    #2180 no. LeetCode solved

---
+++ LeetCode/2180.CountIntegersWithEvenDigitSum.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int countEven(int num) {
+        int count = 0;
+        for(int i = 1; i <= num; i++) {
+            int sum = 0;
+            int n = i;
+            while(n > 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+            if(sum % 2 == 0) count++;
+        }
+        return count;
+    }
+};
+
+int main(){
+    int num = 30;
+    Solution sol;
+    cout << sol.countEven(num);
+    return 0;
+}
