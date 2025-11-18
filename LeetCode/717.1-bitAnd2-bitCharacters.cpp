43d4e5d4b9f9771a47b094c767782610a62d5f13
Roshan Patel <roshanpatel12309@gmail.com>
2025-11-18 06:46:24 +0530

    #717 no. LeetCode solved

---
+++ LeetCode/717.1-bitAnd2-bitCharacters.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+ public:
+  bool isOneBitCharacter(vector<int>& bits) {
+    const int n = bits.size();
+
+    int i = 0;
+    while (i < n - 1)
+      if (bits[i] == 0)
+        i += 1;
+      else
+        i += 2;
+
+    return i == n - 1;
+  }
+};
+
+int main() {
+  Solution sol;
+  vector<int> bits = {1, 0, 0};
+  cout << sol.isOneBitCharacter(bits) << endl; // Output: true
+  return 0;
+}
