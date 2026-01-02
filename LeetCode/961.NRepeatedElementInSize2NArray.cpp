c04b2577ee743d5ace33fd97039f21bd97016810
Roshan Patel <roshanpatel12309@gmail.com>
2026-01-02 06:07:12 +0530

    #961 no. LeetCode solved

--- LeetCode/961.NRepeatedElementInSize2NArray.cpp
+++ LeetCode/961.NRepeatedElementInSize2NArray.cpp
@@ -7,14 +7,3 @@ int repeatedNTimes(vector<int>& nums) {
     }
     return -1;
 }
-
-// Without Using STL (mathematical approach)
-int repeatedNTimes(vector<int>& nums) {
-    for (int i = 0; i < nums.size(); i++) {
-            if (i >= 1 && nums[i] == nums[i - 1])
-                return nums[i];
-            if (i >= 2 && nums[i] == nums[i - 2])
-                return nums[i];
-    }
-        return -1;
-    }
