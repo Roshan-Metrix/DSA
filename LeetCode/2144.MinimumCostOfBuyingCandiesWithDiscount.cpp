78ebfd5b89a9d3426b43ded38e42d8442b6f8257
Roshan Patel <roshanpatel12309@gmail.com>
2026-01-26 09:00:40 +0530

    #2144 no. LeetCode solved

---
+++ LeetCode/2144.MinimumCostOfBuyingCandiesWithDiscount.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(cost.begin(), cost.end(), greater<int>());
+
+        int result = 0;
+        for (int i = 0; i < cost.size(); i++) {
+            // Every 3rd candy is free
+            if ((i + 1) % 3 != 0) {
+                result += cost[i];
+            }
+        }
+        return result;
+    }
+};
