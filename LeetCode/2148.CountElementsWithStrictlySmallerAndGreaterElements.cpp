c2cc33d6c8d2bd16978eb9e48af578ddbaba9b51
Roshan Patel <roshanpatel12309@gmail.com>
2026-01-31 08:03:21 +0530

    #2148 no. LeetCode solved

---
+++ LeetCode/2148.CountElementsWithStrictlySmallerAndGreaterElements.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countElements(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int count = 0;
+
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] > nums.front() && nums[i] < nums.back()) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
