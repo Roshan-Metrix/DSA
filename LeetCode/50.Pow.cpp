2478b08f2defe927346df817772d14a7cec6e0d3
Roshan Patel <roshanpatel12309@gmail.com>
2026-02-13 22:20:45 +0530

    #50 no. LeetCode solved

--- LeetCode/50.Pow.cpp
+++ LeetCode/50.Pow.cpp
@@ -1,31 +1,12 @@
-// We use Binary Exponential method to tackle this problem
-
-#include<iostream>
-using namespace std;
-
 class Solution {
-public:
-    double myPow(double x, int n) {
-        double result = 1;
-        long long power = n;
-
-        if (power < 0) {
-            x = 1 / x;
-            power = -power;
-        }
-
-        for (int i = 0; i < power; ++i) {
-            result *= x;
-        }
-
-        return result;
-    }
+ public:
+  double myPow(double x, long n) {
+    if (n == 0)
+      return 1;
+    if (n < 0)
+      return 1 / myPow(x, -n);
+    if (n % 2 == 1)
+      return x * myPow(x, n - 1);
+    return myPow(x * x, n / 2);
+  }
 };
-
-
-int main(){
-    double base = 2;
-    int power = -3; 
-    cout << myPow(base, power);
-    return 0;
-}
