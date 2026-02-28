db9748e81a05d928ebd179c3405c55dd925413a9
Roshan Patel <roshanpatel12309@gmail.com>
2026-02-28 08:32:32 +0530

    #1680 no. LeetCode solved

---
+++ LeetCode/1680.ConcatenationOfConsecutiveBinaryNumbers.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        const int MOD = 1e9 + 7;
+        long long result = 0;
+        int length = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if ((i & (i - 1)) == 0) {
+                length++;
+            }
+
+            result = ((result << length) + i) % MOD;
+        }
+
+        return result;
+    }
+};
