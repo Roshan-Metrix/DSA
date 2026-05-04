38f54afc780e2d64713f59e71b56277e33045018
Roshan Patel <roshanpatel12309@gmail.com>
2026-05-04 13:22:44 +0545

    #303 no. LeetCode solved

--- LeetCode/303.RangeSumQuery-Immutable.cpp
+++ LeetCode/303.RangeSumQuery-Immutable.cpp
@@ -2,18 +2,24 @@ class NumArray {
 public:
     NumArray(vector<int>& nums) {
         int n = nums.size();
-        prefix.resize(n+1);
-        for(int i =  0; i < nums.size(); i++){
-            prefix[i+1] = prefix[i] + nums[i];
+
+        prefix.resize(n);
+        prefix[0] = nums[0];
+
+        for(int i = 1; i < n; i++){
+            prefix[i] = prefix[i - 1] + nums[i];
         }
     }
     
     int sumRange(int left, int right) {
-        return prefix[right+1] - prefix[left];
+        if(left == 0){
+            return prefix[right];
+        }
+        return prefix[right] - prefix[left - 1];
     }
 
-    private:
-      vector<int> prefix;
+private:
+    vector<int> prefix;
 };
 
 /**
