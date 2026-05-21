647ff54b0fcde8914becb5c9f37fad18f8830d75
Roshan Patel <roshanpatel12309@gmail.com>
2026-05-21 22:48:47 +0545

    #3043 no. LeetCode solved

---
+++ LeetCode/3043.FindTheLengthOfTheLongestCommonPrefix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        unordered_set<int> prefixes;
+
+        // Store all prefixes from arr1
+        for (int num : arr1) {
+            while (num > 0) {
+                prefixes.insert(num);
+                num /= 10;
+            }
+        }
+
+        int ans = 0;
+
+        // Check prefixes in arr2
+        for (int num : arr2) {
+            while (num > 0) {
+                if (prefixes.count(num)) {
+                    ans = max(ans, (int)to_string(num).size());
+                    break;
+                }
+                num /= 10;
+            }
+        }
+
+        return ans;
+    }
+};
