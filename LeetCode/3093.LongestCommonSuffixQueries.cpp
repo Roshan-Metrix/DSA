6c9c9e67edc89fcddd893fe8e698439a1a9f5fa6
Roshan Patel <roshanpatel12309@gmail.com>
2026-05-28 12:10:51 +0545

    #3093 no. LeetCode solved

--- LeetCode/3093.LongestCommonSuffixQueries.cpp
+++ LeetCode/3093.LongestCommonSuffixQueries.cpp
@@ -1,61 +1,64 @@
-class Trie {
-private:
-    const int inf = 1 << 30;
-    Trie* children[26];
-    int length = inf;
-    int idx = inf;
-
-public:
-    Trie() {
-        for (int i = 0; i < 26; ++i) {
-            children[i] = nullptr;
-        }
-    }
-
-    void insert(string w, int i) {
-        Trie* node = this;
-        if (node->length > w.length()) {
-            node->length = w.length();
-            node->idx = i;
-        }
-        for (int k = w.length() - 1; k >= 0; --k) {
-            int idx = w[k] - 'a';
-            if (node->children[idx] == nullptr) {
-                node->children[idx] = new Trie();
-            }
-            node = node->children[idx];
-            if (node->length > w.length()) {
-                node->length = w.length();
-                node->idx = i;
-            }
-        }
-    }
-
-    int query(string w) {
-        Trie* node = this;
-        for (int k = w.length() - 1; k >= 0; --k) {
-            int idx = w[k] - 'a';
-            if (node->children[idx] == nullptr) {
-                break;
-            }
-            node = node->children[idx];
-        }
-        return node->idx;
-    }
-};
-
 class Solution {
+    struct TrieNode {
+        int children[26];
+        int bestLen;
+        int bestIdx;
+        
+        TrieNode() {
+            fill(begin(children), end(children), -1);
+            bestLen = 1e9;
+            bestIdx = 1e9;
+        }
+    };
+
 public:
     vector<int> stringIndices(vector<string>& wordsContainer, vector<string>& wordsQuery) {
-        Trie* trie = new Trie();
-        for (int i = 0; i < wordsContainer.size(); ++i) {
-            trie->insert(wordsContainer[i], i);
+        vector<TrieNode> trie;
+        trie.emplace_back();
+        
+        for (int i = 0; i < wordsContainer.size(); i++) {
+            int len = wordsContainer[i].length();
+            int curr = 0;
+            
+            if (len < trie[curr].bestLen || (len == trie[curr].bestLen && i < trie[curr].bestIdx)) {
+                trie[curr].bestLen = len;
+                trie[curr].bestIdx = i;
+            }
+            
+            for (int j = len - 1; j >= 0; j--) {
+                int charIdx = wordsContainer[i][j] - 'a';
+                
+                if (trie[curr].children[charIdx] == -1) {
+                    trie[curr].children[charIdx] = trie.size();
+                    trie.emplace_back();
+                }
+                
+                curr = trie[curr].children[charIdx];
+                
+                if (len < trie[curr].bestLen || (len == trie[curr].bestLen && i < trie[curr].bestIdx)) {
+                    trie[curr].bestLen = len;
+                    trie[curr].bestIdx = i;
+                }
+            }
         }
-        int n = wordsQuery.size();
-        vector<int> ans(n);
-        for (int i = 0; i < n; ++i) {
-            ans[i] = trie->query(wordsQuery[i]);
+        
+        vector<int> ans;
+        ans.reserve(wordsQuery.size());
+        
+        for (const string& query : wordsQuery) {
+            int curr = 0;
+            int len = query.length();
+            
+            for (int j = len - 1; j >= 0; j--) {
+                int charIdx = query[j] - 'a';
+                if (trie[curr].children[charIdx] == -1) {
+                    break;
+                }
+                curr = trie[curr].children[charIdx];
+            }
+            ans.push_back(trie[curr].bestIdx);
         }
+        
         return ans;
     }
-};
+}; 
